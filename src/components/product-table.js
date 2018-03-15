4892fd840ca42ae52d8b913e7c4dac20a4a1b671
dayamll <dayamllerenacastro@gmail.com>
2018-03-15 00:12:47 -0500

    Changing class for arrows functions

--- src/components/product-table.js
+++ src/components/product-table.js
@@ -1,36 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import ProductCategoryRow from './product-category-row';
 import ProductRow from './product-row';
 
-class ProductTable extends Component {
-  render() {
-    //Esto es la propiedad products que es del tipo array
-    const {products} = this.props;
-    // Seteando valores
-    const rows = [];
-    let lastCategory = null;
-    //Creando la categoria 
-    products.forEach(product => {
-      if (product.category !== lastCategory) {
-        rows.push(
-          <ProductCategoryRow category={product.category} key={product.category} />
-        );
-      }
-      rows.push(<ProductRow product={product} key={product.name} />);
-      lastCategory = product.category;
-    });
-    return (
-      <table className = "table">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Price</th>
-          </tr>
-        </thead>
-        <tbody>{rows}</tbody>
-      </table>
-    )
-  }
+const ProductTable = ({ products }) => {
+  // Seteando valores
+  const rows = [];
+  let lastCategory = null;
+
+  return (
+    <table className="table">
+      {/* <thead>
+        <tr>
+          <th>Name</th>
+          <th>Price</th>
+        </tr>
+      </thead> */}
+      <tbody>{rows}</tbody>
+    </table>
+  )
 }
 
 export default ProductTable
